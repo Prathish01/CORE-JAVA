149c28dbb70e3073d0043fd305418a327c44b7ce
Prathish S <prathish472@gmail.com>
2026-03-24 08:04:22 +0530

    Update linkedhashmapstringtomaptofindthefirst.java

--- Collections/hashmap/linkedhashmapstringtomaptofindthefirst.java
+++ Collections/hashmap/linkedhashmapstringtomaptofindthefirst.java
@@ -22,12 +22,14 @@ map.put(c,map.getOrDefault(c, 0)+1);
 }
 
 }
+  
 for(Map.Entry<Character, Integer> entry : map.entrySet())
 {
 if(entry.getValue() > 1)
 {
 System.out.println(entry.getKey()  + " "  +  entry.getValue());	
 }
+  
 }
 }
 }
