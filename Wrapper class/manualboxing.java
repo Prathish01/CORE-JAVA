bbcb9f206ac4cc6334b8808b0087b78c0c2510ec
Prathish S <prathish472@gmail.com>
2026-04-02 08:34:47 +0530

    Create manualboxing.java

---
+++ Wrapper class/manualboxing.java
@@ -0,0 +1,18 @@
+package wrapper;
+
+public class autoboxing 
+{
+public static void main(String [] args)
+{
+int qw = 10;
+Integer er = Integer.valueOf(qw);
+System.out.println(er);
+
+
+}
+}
+
+
+
+
+the process of changing primitive data into object class manually is calles manual boxing
