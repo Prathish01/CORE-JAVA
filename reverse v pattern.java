b5c57d01261ea543959e384c04f98e7421a79b65
Prathish S <prathish472@gmail.com>
2026-05-01 14:48:16 +0530

    Update reverse v pattern.java

--- reverse v pattern.java
+++ reverse v pattern.java
@@ -1,5 +1,6 @@
 package patternproblems;
 
+
 public class upsidev 
 {
 	public static void main(String [] args)
@@ -25,7 +26,7 @@ public class upsidev
 }
 }
 
-output
+output:
 
               *               
              * *              
