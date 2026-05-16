d2c60f3619bfeba31585d1ff83ee2ec1b9eb2437
Prathish S <prathish472@gmail.com>
2026-05-16 14:01:44 +0530

    Update getandset.java

--- Thread/getandset.java
+++ Thread/getandset.java
@@ -9,7 +9,11 @@ public class getandset1 implements Runnable
         System.out.println("Priority after change (child thread): " + Thread.currentThread().getPriority());
     }
 
+
+    
     public static void main(String[] args)
+
+    
     {
         System.out.println("Default main thread priority: " + Thread.currentThread().getPriority());
         Thread.currentThread().setPriority(1);
@@ -18,5 +22,9 @@ public class getandset1 implements Runnable
         getandset1 ti = new getandset1();
         Thread t1 = new Thread(ti);
         t1.start();
+
+        
     }
+
+    
 }
