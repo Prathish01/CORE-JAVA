4395c0d857faf1391604e17a866160000d0bba85
Prathish S <prathish472@gmail.com>
2026-05-16 14:01:25 +0530

    Update creation.java

--- Thread/creation.java
+++ Thread/creation.java
@@ -1,5 +1,8 @@
 package thread;
 import java.lang.Thread;
+
+
+
 public class getnamethread 
 {
 public static void main(String [] args)
@@ -7,3 +10,8 @@ public static void main(String [] args)
 System.out.println(Thread.currentThread().getName());
 }
 }
+
+
+
+
+
