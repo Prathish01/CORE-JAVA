32ba68d6666a5437ff0088cdf51a821616171826
Prathish S <prathish472@gmail.com>
2026-05-17 18:08:14 +0530

    Update maxarray.java

--- Array and programs/maxarray.java
+++ Array and programs/maxarray.java
@@ -4,16 +4,22 @@ class we12
 	{
 		int[] ar1 ={1,2,3,4,5};
 	int max = ar1[1];
+		
 	for(int i =0;i<=ar1.length-1;i++)
 	{
+		
 	if(max <ar1[i])
+		
 	{
 		max = ar1[i];
+		
 		
 	}
 	
 	}
+		
 	System.out.println(max);	
+		
 	}
 	
 }
