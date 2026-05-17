df88b8c078d66446fafb4a11378b8461a2ba82dc
Prathish S <prathish472@gmail.com>
2026-05-17 18:06:38 +0530

    Update convert arr to arraylist.java

--- Array and programs/convert arr to arraylist.java
+++ Array and programs/convert arr to arraylist.java
@@ -5,16 +5,21 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
+
 public class arraytoarraylist 
 {
+  
 public static void main(String [] args)
 {
 Integer[] arr = {10,40,30,20,45,50};	
 List<Integer> list = new ArrayList<>(Arrays.asList(arr));
 
+  
 Collections.sort(list);
 
+  
 System.out.println(list);
+  
 }
 }
 
