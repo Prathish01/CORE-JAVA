46e2f67ba91b14511bdb86e5e1507354b15abf1e
Prathish S <prathish472@gmail.com>
2026-05-17 18:07:01 +0530

    Update converting into list.java

--- Array and programs/converting into list.java
+++ Array and programs/converting into list.java
@@ -6,18 +6,25 @@ import java.util.Arrays;
 
 public class intarraytolist 
 {
-public static void main(String [] args)
+public static void main(String [] args) 
 {
+	
 	System.out.println("creating arrays");
 	int[] arr = {10,20,30,40,50};
+	
 	System.out.println(Arrays.toString(arr));
 	List li = new ArrayList<>();
+	
 	System.out.println("converting into list");
 	for(int m1 :arr) 
+		
 	{
+		
 		li.add(m1);
 		
 	}
+	
 	System.out.println(li);
+	
 }
 }
