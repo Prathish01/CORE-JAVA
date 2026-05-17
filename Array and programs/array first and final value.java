f18f4c60f45cba0cd77d8fc3b09e15e6f9ca258e
Prathish S <prathish472@gmail.com>
2026-05-17 18:06:22 +0530

    Update array first and final value.java

--- Array and programs/array first and final value.java
+++ Array and programs/array first and final value.java
@@ -10,15 +10,20 @@ class we12
         int[] ar1 = new int[size];
 		//int max = ar1[1];
 
+
+		
         System.out.println("enter values");
        for(int i = 0; i < ar1.length; i++)
          {
 			ar1[i] = scn.nextInt();	
 			}
-       
+
+
+		
             System.out.println("-------------------");
 			//int index = scn.nextInt();
 		System.out.println(ar1[0]);
 
+		
 	}
 }
