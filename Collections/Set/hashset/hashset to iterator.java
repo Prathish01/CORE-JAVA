df4535de812c7de713eca774749453b1eb887283
Prathish S <prathish472@gmail.com>
2026-05-18 19:17:09 +0530

    Update hashset to iterator.java

--- Collections/Set/hashset/hashset to iterator.java
+++ Collections/Set/hashset/hashset to iterator.java
@@ -8,19 +8,28 @@ public class arraytoset
 {
 public static void main(String [] args)
 {
+  
 System.out.println("creating array");
+  
 int[] arr = {10,20,30,40,50,60,30,50,10,20};
+  
 System.out.println("creating a set");
+  
 Set st = new HashSet();
+  
 for(int a : arr)
+  
 {
 st.add(a);	
 }
 
 Iterator itr = st.iterator();
+  
 while(itr.hasNext())
 {
+  
 System.out.println(itr.next() + " ");	
+  
 }
 
 System.out.println(st);
