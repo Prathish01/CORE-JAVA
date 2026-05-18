bef0afabb430786601286c24fb9b8493257dbcb2
Prathish S <prathish472@gmail.com>
2026-05-18 19:16:09 +0530

    Update array to list.java

--- Collections/Set/hashset/array to list.java
+++ Collections/Set/hashset/array to list.java
@@ -5,17 +5,26 @@ import java.util.Set;
 
 public class arraytoset 
 {
+  
 public static void main(String [] args)
 {
+  
 System.out.println("creating array");
+  
 int[] arr = {10,20,30,40,50,60,30,50,10,20};
+  
 System.out.println("creating a set");
+  
 Set st = new HashSet();
+  
 for(Integer a : arr)
+  
 {
 st.add(a);	
 }
+  
 System.out.println(st);
+  
 }
 }
 
