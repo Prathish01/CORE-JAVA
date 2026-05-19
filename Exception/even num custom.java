6418ada1ad3060c434eeec6445470fbfdd5c7d74
Prathish S <prathish472@gmail.com>
2026-05-19 17:37:50 +0530

    Update even num custom.java

--- Exception/even num custom.java
+++ Exception/even num custom.java
@@ -2,29 +2,37 @@ package customisedexception;
 import java.util.Scanner;
 public class mainmethod 
 {
+	
 public static void main(String [] args)
+	
 {
 Scanner scn = new Scanner(System.in);
 System.out.println("enter num");
+	
 int num = scn.nextInt();
 
 if(num%2==0)
 {
 	System.out.println(" even number " +num);
+	
 throw new  EvenNumberException("even number")	;
 }
 else
 {
+	
 	System.out.println("not an even number " +num);
+	
 	throw new NotAnEvenNumberException(" not an even number");
 	
 }
 }
+	
 }
 
 package customisedexception;
 
 public class EvenNumberException extends RuntimeException
+	
 {
 public EvenNumberException(String message)
 {
@@ -35,7 +43,9 @@ super(message)	;
 
 package customisedexception;
 
+
 public class NotAnEvenNumberException extends RuntimeException 
+	
 {
 public NotAnEvenNumberException(String message)
 {
