5777b1db1fe6005b8b17768090fba8560d1ecaf3
Prathish S <prathish472@gmail.com>
2026-05-19 17:39:55 +0530

    Update reverseorderstack.java

--- Collections/Stack/reverseorderstack.java
+++ Collections/Stack/reverseorderstack.java
@@ -3,17 +3,22 @@ package stack;
 import java.util.Collections;
 import java.util.Stack;
 
+
 public class stackcreation 
 {
 public static void main(String [] args)
+	
 {
 	Stack sta = new Stack();
 	sta.add(10);
 	sta.add(22);
+	
 	sta.add(7);
 	sta.add(444);
 	sta.add(66);
+	
 	sta.add(565);
+	
 	Collections.sort(sta,Collections.reverseOrder());
 	System.out.println(sta);
 	
