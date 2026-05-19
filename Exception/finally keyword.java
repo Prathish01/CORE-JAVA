a6ff201b2f3dc0d8779cc49d2701bcfa1fb3f4e9
Prathish S <prathish472@gmail.com>
2026-05-19 17:38:05 +0530

    Update finally keyword.java

--- Exception/finally keyword.java
+++ Exception/finally keyword.java
@@ -7,21 +7,26 @@ public static void main(String [] args)
 System.out.println(1);
 System.out.println(2);
 try {
+	
 System.out.println(3);
 System.out.println(4/0);
 }
+	
 catch(Exception e)
 {
 System.out.println(5);
 System.out.println(6);
+	
 System.out.println(7);
 }
 finally
 {
 System.out.println("start");
 System.out.println("end");
+	
 System.out.println("finally block");
 }
+
 	
 }
 }
