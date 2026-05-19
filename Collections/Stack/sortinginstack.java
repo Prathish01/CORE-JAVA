60b11143dd2f5c5c0a9d287339e996836294c506
Prathish S <prathish472@gmail.com>
2026-05-19 17:40:12 +0530

    Update sortinginstack.java

--- Collections/Stack/sortinginstack.java
+++ Collections/Stack/sortinginstack.java
@@ -12,12 +12,17 @@ public static void main(String [] args)
 	sta.add(4545);
 	sta.add(7);
 	sta.add(454);
+	
 	sta.add(66);
+	
 	sta.add(1);
 	Collections.sort(sta);
+	
 	System.out.println(sta);
+	
 	System.out.println();
 	System.out.println("descending order");
+	
 	Collections.reverse(sta);
 	System.out.println(sta);
 	}
