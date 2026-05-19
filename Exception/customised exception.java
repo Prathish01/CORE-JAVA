e852334b7b6c298d31a85c503d90e8df092a3288
Prathish S <prathish472@gmail.com>
2026-05-19 17:36:20 +0530

    Update customised exception.java

--- Exception/customised exception.java
+++ Exception/customised exception.java
@@ -1,10 +1,13 @@
 package customisedexception;
 
 public class EvenNumberException extends RuntimeException
+    
 {
 public EvenNumberException(String message)
+    
 {
 super(message)	;
+    
 }
 }
 
@@ -12,9 +15,11 @@ super(message)	;
 package customisedexception;
 
 public class NotAnEvenNumberException extends RuntimeException 
+    
 {
 public NotAnEvenNumberException(String message)
 {
+    
 super(message);	
 }
 }
@@ -29,13 +34,16 @@ public class mainmethod {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("enter num");
+        
         int num = scn.nextInt();
 
         if (num % 2 == 0) {
             System.out.println("even number " + num);
             throw new EvenNumberException("even number");
         } else {
+            
             System.out.println("not an even number " + num);
+            
             throw new NotAnEvenNumberException("not an even number");
         }
     }
