50e5c51eadb6eeedd95b62c1336ef2cf14a4a4a0
Prathish S <prathish472@gmail.com>
2026-05-21 17:47:41 +0530

    Update setintoString.java

--- Collections/linkedhashset/setintoString.java
+++ Collections/linkedhashset/setintoString.java
@@ -5,8 +5,11 @@ import java.util.Set;
 
 public class settostring 
 {
+    
 public static void main(String [] args)
+    
 {
+    
 System.out.println("creating linked hashset");
 
 Set set = new LinkedHashSet();
@@ -15,15 +18,21 @@ set.add("hello");
 set.add("world");
 set.add(1.2);
 set.add(4);
+    
 set.add(6);
 set.add("rono");
 System.out.println("converting set into string");
 
 String result = "";
-for (Object obj : set) {
+    
+for (Object obj : set)
+    {
     result += obj.toString() + " ";
+        
 }
+    
 System.out.println(result);
+    
 }
 }
 
