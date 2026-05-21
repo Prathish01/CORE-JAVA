9f76e747e19f39fdf85571b5d8f83ce6ce54d274
Prathish S <prathish472@gmail.com>
2026-05-21 17:47:13 +0530

    Update object array into set.java

--- Collections/linkedhashset/object array into set.java
+++ Collections/linkedhashset/object array into set.java
@@ -5,17 +5,25 @@ import java.util.Set;
 
 public class arraytoset 
 {
+  
 public static void main(String [] args)
+  
 {
 Object[] i = {10,20,30,40,50,"mesi",10.34};
+  
 
 System.out.println("convertig array into set");
 
+  
 Set  set = new LinkedHashSet();
 for(Object u :i)
 {
+  
 set.add(u);	
+  
 }
+  
 System.out.println(set);
+  
 }
 }
