8dca1e6a9dac0ae3b5fcadb2c174f10049a57c56
Prathish S <prathish472@gmail.com>
2026-05-21 17:45:35 +0530

    Update foreach.java

--- Collections/linkedhashset/foreach.java
+++ Collections/linkedhashset/foreach.java
@@ -4,22 +4,31 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-public class iterator {
+public class iterator 
+{
 	public static void main(String [] args)
+	
 	{
+		
 	Set rt = new LinkedHashSet();
 	rt.add("hello");
 	rt.add("world");
 	rt.add("long ball");
+		
 	rt.add("kio");
 	rt.add("hiio");
 	rt.add("messi");
+		
 	System.out.println(rt);
 	
 	for(Object s : rt)
+		
 	{
+		
 		System.out.println(s);
+		
 	}
+		
 	}
 		
 
