e89666f0c82bd71bb0bbfeb2e6c30112cb70bb97
Prathish S <prathish472@gmail.com>
2026-05-21 17:39:07 +0530

    Update dicerollguess.java

--- Mini Games/dicerollguess.java
+++ Mini Games/dicerollguess.java
@@ -3,12 +3,19 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.random.*;
 public class stonepaperscissor1 
+	
 {
+	
 public static void main(String [] args)
+	
 {
+	
 Scanner scn = new Scanner(System.in);
+	
 Random rand = new Random();
+	
 int compchoice = rand.nextInt(6)+1;
+	
 int guess = 0;
 
 
@@ -18,15 +25,22 @@ while(guess !=compchoice)
 	guess = scn.nextInt();
 	if(guess == compchoice)
 	{
+		
 		System.out.println("your guess is correct  : " +guess);
+		
 	}
+		
 	else if(guess>compchoice)
 	{
+		
 		System.out.println("your guess is high");
+		
 	}
 	else
 	{
+		
 		System.out.println("your guess is low");
+		
 	}
 }
 scn.close();
