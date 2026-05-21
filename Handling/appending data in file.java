db5882a60218310449964501047bf2703a1dba7e
Prathish S <prathish472@gmail.com>
2026-05-21 17:41:26 +0530

    Update appending data in file.java

--- Handling/appending data in file.java
+++ Handling/appending data in file.java
@@ -2,11 +2,16 @@ package filehandlind;
 import java.io.*;
 public class append_keyword_in_jacva 
 {
+	
 public static void main(String [] args) throws IOException
+	
 {
+	
 	File handling = new File("E:\\javaclasses\\handling\\hello.txt");
+	
 	FileWriter fa = new FileWriter(handling);
 	fa.append("hi ");
+	
 	fa.flush();
 }
 }
