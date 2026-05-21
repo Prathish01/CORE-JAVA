089e293a450afc364c1bd94ded1b75dee5aa8744
Prathish S <prathish472@gmail.com>
2026-05-21 17:42:19 +0530

    Update file deletion.java

--- Handling/file deletion.java
+++ Handling/file deletion.java
@@ -2,14 +2,24 @@ package filehandlind;
 import java.io.File;
 import java.io.IOException;
 
+
 public class filecreation {
+    
     public static void main(String[] args) throws IOException {
+        
         File handling = new File("E:\\javaclasses\\handling\\hello.txt");
+        
         handling.createNewFile();
         if (handling.delete()) {
+            
             System.out.println("File deleted successfully");
-        } else {
+            
+        }
+        else 
+        {
+            
             System.out.println("Failed to delete the file");
+            
         }
     }
 }
