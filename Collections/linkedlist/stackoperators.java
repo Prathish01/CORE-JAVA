a490659331ae7647a6189230e1b1e5c033e74872
Prathish S <prathish472@gmail.com>
2026-05-22 21:31:01 +0530

    Update stackoperators.java

--- Collections/linkedlist/stackoperators.java
+++ Collections/linkedlist/stackoperators.java
@@ -12,18 +12,26 @@ public static void main(String [] args)
 {
 LinkedList<Integer> num = new LinkedList<>();
 num.add(1);
+  
 num.add(2);
+  
 num.add(3);
+  
 num.add(4);
+  
 num.add(5);
 
 num.push(12);
 int peek = num.peek();
+  
 System.out.println("while peek :" +peek);
 System.out.println("after peek" +num);
 int pop = num.pop();
+
+  
 System.out.println("while pop :" +pop);
 System.out.println("after peek" +num);
+  
 }
 }
 
