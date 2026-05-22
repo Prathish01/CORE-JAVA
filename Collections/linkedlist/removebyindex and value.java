b822f7bb21351b6ae0b6b081ce754b42791655ba
Prathish S <prathish472@gmail.com>
2026-05-22 21:29:56 +0530

    Update removebyindex and value.java

--- Collections/linkedlist/removebyindex and value.java
+++ Collections/linkedlist/removebyindex and value.java
@@ -11,10 +11,14 @@ public static void main(String [] args)
 LinkedList<Integer> num = new LinkedList<>();
 num.add(1);
 num.add(2);
+  
 num.add(3);
 num.add(4);
+  
 num.remove(Integer.valueOf(1));
 num.remove(2);
+  
 System.out.println(num);
+  
 }
 }
