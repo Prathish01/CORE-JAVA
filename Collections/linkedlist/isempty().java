f5a2286aee01a0c1006983f4f52e5a8ba74814b5
Prathish S <prathish472@gmail.com>
2026-05-22 21:27:44 +0530

    Update isempty().java

--- Collections/linkedlist/isempty().java
+++ Collections/linkedlist/isempty().java
@@ -8,10 +8,12 @@ LinkedList<Integer> ll = new LinkedList<Integer>();
 ll.add(00);
 ll.add(10);
 ll.add(20);
+  
 ll.add(30);
 ll.add(40);
 ll.add(50);
 ll.add(50);
+  
 ll.add(2,30);
 
 
@@ -19,5 +21,6 @@ System.out.println(ll.isEmpty());
 
 }
 }
+
 it returns boolean value with respect to the list 
   if the list id empty IT PROVIDES true if the list is not empty returns false
