439897fe046d479a17bb0f9f700cee4b3b62803e
Prathish S <prathish472@gmail.com>
2026-05-22 21:25:46 +0530

    Update add() &add(index,value).java

--- Collections/linkedlist/add() &add(index,value).java
+++ Collections/linkedlist/add() &add(index,value).java
@@ -2,6 +2,7 @@ package list;
 import java.util.LinkedList;
 public class arraylist 
 {
+  
 public static void main(String [] args)
 {
 LinkedList<Integer> ll = new LinkedList<Integer>();
@@ -9,26 +10,36 @@ ll.add(00);
 ll.add(10);
 ll.add(20);
 ll.add(30);
+  
 ll.add(40);
 ll.add(50);
 
+  
 }
 }
+
 
 package list;
 import java.util.LinkedList;
 public class arraylist 
+  
 {
 public static void main(String [] args)
+  
 {
 LinkedList<Integer> ll = new LinkedList<Integer>();
 ll.add(00);
 ll.add(10);
+  
 ll.add(20);
+  
 ll.add(30);
 ll.add(40);
+  
 ll.add(50);
+  
 ll.add(2,30);
+  
 System.out.println(ll);
 
 }
