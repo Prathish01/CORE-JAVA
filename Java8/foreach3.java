9d6733796a1def4d2acf047f4cb2724f72a98317
Prathish S <prathish472@gmail.com>
2026-05-23 13:23:00 +0530

    Update foreach3.java

--- Java8/foreach3.java
+++ Java8/foreach3.java
@@ -4,9 +4,16 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class stram 
+  
 {
+
+  
 public static void main(String [] args)
+  
 {
+  
 List <Integer> li = Arrays.asList(8,4,5,6,7,3,4);
+  
 Integer hi = li.stream().reduce(0,(c,e) -> c+e);
+  
 System.out.println(hi);
