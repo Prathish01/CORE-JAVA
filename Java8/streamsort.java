a933081eb0c16f48dd7b3b4cd53adaad06d39760
Prathish S <prathish472@gmail.com>
2026-05-23 13:27:03 +0530

    Update streamsort.java

--- Java8/streamsort.java
+++ Java8/streamsort.java
@@ -1,12 +1,18 @@
 package java8;
 
 import java.util.Arrays;
+
 import java.util.List;
+
 import java.util.stream.Stream;
 
+
 public class stram 
+  
 {
+  
 public static void main(String [] args)
+  
 {
 List <Integer> li = Arrays.asList(8,4,5,6,7,3,4); 
 Stream<Integer> data = li.stream();
@@ -27,6 +33,7 @@ List <Integer> li = Arrays.asList(8,4,5,6,7,3,4);
 
 Stream<Integer> data = li.stream();
 
+  
 
 Stream<Integer> data2 = data.sorted();
 data2.forEach(n -> System.out.println(n * 2));
