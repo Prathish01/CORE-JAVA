7291816231740f188f393990728cb3b8832981cb
Prathish S <prathish472@gmail.com>
2026-05-24 12:01:55 +0530

    Update and rename Method overriding 3 to Method overriding 3.java

--- OOPS concept/Method overriding 3.java
+++ OOPS concept/Method overriding 3.java
@@ -2,16 +2,23 @@ class college {
  public void move() {
   System.out.println("College is open");
  }
+ 
 }
-class univ extends college {
+class univ extends college 
+{
  public void move() {
+  
   System.out.println("University is open too");
+  
  }
 }
-public class stud {
+public class stud
+ {
  public static void main(String args[]) {
+  
   college a = new college();
   college b = new univ();
+  
   a.move();
   b.move();
  }
