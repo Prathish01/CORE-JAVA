8a98c341e2a02edc9614f051438498431414395e
Prathish S <prathish472@gmail.com>
2026-05-29 17:45:34 +0530

    Update stackremove.java

--- Collections/Stack/stackremove.java
+++ Collections/Stack/stackremove.java
@@ -8,10 +8,13 @@ public static void main(String [] args)
 {
 	Stack sta = new Stack();
 	sta.add(10);
+	
 	sta.add("messi");
 	sta.add(7);
+	
 	sta.add("ronaldo");
 	sta.add(66);
+	
 	sta.add("arnold");
 	sta.remove(5);
 	
