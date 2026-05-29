01d624c97754a7bfa3962a6a1f8ac37703439846
Prathish S <prathish472@gmail.com>
2026-05-29 17:45:19 +0530

    Update stackpush.java

--- Collections/Stack/stackpush.java
+++ Collections/Stack/stackpush.java
@@ -6,11 +6,14 @@ public class pushstack
 {
 public static void main(String [] args)
 {
+  
 System.out.println("creation of stack");
 Stack st= new Stack();
 st.push("hello");
+  
 st.push("world");
 st.push(10);
+  
 st.push(7);
 System.out.println(st);
 }
