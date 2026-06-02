0192f8fc3ff593eed56da21f2f697e1de822e3f4
Prathish S <prathish472@gmail.com>
2026-06-02 20:04:19 +0530

    Update settoarray.java

--- Collections/treeset/settoarray.java
+++ Collections/treeset/settoarray.java
@@ -10,19 +10,26 @@ public class settoaray
 
 public static void main(String [] args)
 {
+  
 System.out.println("creation of treeset");
 
+  
 Set<Integer> set = new TreeSet<Integer>();
 set.add(10);
 set.add(20);
 set.add(30);
+  
 set.add(40);
 set.add(50);
 set.add(60);
+  
 set.add(70);
+  
 set.add(80);
 set.add(90);
+  
 set.add(100);
+  
 set.add(110);
 set.add(100);
 System.out.println("convert set into array");
