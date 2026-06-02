7ad71a2d4b33a6f3d9568c4b48d4228c061e2b84
Prathish S <prathish472@gmail.com>
2026-06-02 20:08:35 +0530

    Update get() & set() method.java

--- Collections/linkedlist/get() & set() method.java
+++ Collections/linkedlist/get() & set() method.java
@@ -8,16 +8,21 @@ public static void main(String [] args)
 {
   
 LinkedList<Integer> ll = new LinkedList<Integer>();
+  
 ll.add(00);
+  
 ll.add(10);
   
 ll.add(20);
+  
 ll.add(30);
   
 ll.add(40);
+  
 ll.add(50);
   
 ll.add(2,30);
+  
 System.out.println(ll.get(6));
 //System.out.println(ll);
 
