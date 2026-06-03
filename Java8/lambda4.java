305c9087a26c251a2962b7b3d62013a26f295819
Prathish S <prathish472@gmail.com>
2026-06-03 21:26:57 +0530

    Update lambda4.java

--- Java8/lambda4.java
+++ Java8/lambda4.java
@@ -7,17 +7,17 @@ void view (int i);
 	
 }
 
-
-
 package java8;
 
-
 public class adf implements thetre {
-
     public static void main(String[] args) {
 
+		
     	thetre th =(int i) ->
+
+			
     	{
+			
     		System.out.println("you selected" + i);
 
 			
