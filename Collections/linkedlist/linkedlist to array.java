f1d4adbcb23d16b9cefe50a555e0402c04e1b0f6
Prathish S <prathish472@gmail.com>
2026-06-04 17:40:11 +0530

    Update linkedlist to array.java

--- Collections/linkedlist/linkedlist to array.java
+++ Collections/linkedlist/linkedlist to array.java
@@ -7,15 +7,17 @@ import java.util.List;
 public class listtoarray1 
 {
 public static void main(String [] args)
-	
 {
 	System.out.println("creating list");
 	
 	List<String> list = new LinkedList<String>();
 	list.add("mbappe");
+	
 	list.add("thuram");
+
 	
 	list.add("dueu");
+	
 	list.add("konate");
 	
 	System.out.println(list);
