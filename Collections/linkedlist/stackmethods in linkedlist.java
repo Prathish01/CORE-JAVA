24bfe78ee277d91e24c83cbffc80ff8ddd80224d
Prathish S <prathish472@gmail.com>
2026-06-04 17:42:25 +0530

    Update stackmethods in linkedlist.java

--- Collections/linkedlist/stackmethods in linkedlist.java
+++ Collections/linkedlist/stackmethods in linkedlist.java
@@ -8,21 +8,14 @@ public class stackinlinkedlist {
 
      
         stack.push(10);
-        
         stack.push(20);
-        
         stack.push(30);
-        
         stack.push(40);
 
-        
         System.out.println("Top element: " + stack.peek());
-
-        
         
         System.out.println("Popped element: " + stack.pop());
 
-        
         System.out.println("Stack after pop: " + stack);
         
     }
