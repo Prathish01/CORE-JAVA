175c0ebb93919eb694bf03d2692d43c066a556bd
Prathish S <prathish472@gmail.com>
2026-06-05 21:29:30 +0530

    Update xpattern.java

--- xpattern.java
+++ xpattern.java
@@ -6,10 +6,15 @@ public class xpattern
 public static void main(String [] args)
 {
 int n=5;
+	
 for(int i =1;i<=n;i++)
+	
 {
+	
 for(int j=1;j<=n*2;j++)
+	
 {
+	
 	if(j == i || j==n*2-1-i+1)
 	{
 		System.out.print("*");
