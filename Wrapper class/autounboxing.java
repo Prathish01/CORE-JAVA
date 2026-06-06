fd1bada665f11771093ec40e7dfa51e7b1c2386f
Prathish S <prathish472@gmail.com>
2026-06-06 21:40:49 +0530

    Update autounboxing.java

--- Wrapper class/autounboxing.java
+++ Wrapper class/autounboxing.java
@@ -2,16 +2,22 @@ package wrapper;
 
 public class autoboxing 
 {
+  
 public static void main(String [] args)
+  
 {
+  
 int a = 10;
 Integer num = a;
+  
 System.out.println(a);
 
 System.out.println("auto unboxing");
 
 Double num1 = 10.56;
+  
 double b = num1;
+  
 System.out.println(b);
 
 
