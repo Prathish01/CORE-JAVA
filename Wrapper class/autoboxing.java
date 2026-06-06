ba52e1eaf82c159bffca37726b109f3cf7172182
Prathish S <prathish472@gmail.com>
2026-06-06 21:40:29 +0530

    Update autoboxing.java

--- Wrapper class/autoboxing.java
+++ Wrapper class/autoboxing.java
@@ -4,11 +4,15 @@ public class autoboxing
 {
 public static void main(String [] args)
 {
+  
 int a = 10;
 Integer num = a;
+  
 System.out.println(a);
+  
 
 System.out.println("auto unboxing");
+  
 }
 }
 
