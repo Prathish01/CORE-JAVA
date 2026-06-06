03122cc82ed3e16e89824e5c1418694adc988cfa
Prathish S <prathish472@gmail.com>
2026-06-06 21:41:06 +0530

    Update manualboxing.java

--- Wrapper class/manualboxing.java
+++ Wrapper class/manualboxing.java
@@ -2,10 +2,15 @@ package wrapper;
 
 public class autoboxing 
 {
+  
 public static void main(String [] args)
+  
 {
+  
 int qw = 10;
+  
 Integer er = Integer.valueOf(qw);
+  
 System.out.println(er);
 
 
