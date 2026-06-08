1b208d7a5fbc16bf223129fa32f8680a690d7cca
Prathish S <prathish472@gmail.com>
2026-06-08 17:51:32 +0530

    Update Scanner class.java

--- Scanner class.java
+++ Scanner class.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 class program14
 {
+	
 	public static void main(String [] args)
 	{
+		
 		Scanner scn = new Scanner(System.in);
 		System.out.println ("ENTER salary");
 		double salary = scn.nextDouble();
