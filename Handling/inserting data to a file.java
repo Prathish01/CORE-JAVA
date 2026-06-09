cda274e23268baa04555682371dbdd5970b485ce
Prathish S <prathish472@gmail.com>
2026-06-09 17:31:20 +0530

    Update inserting data to a file.java

--- Handling/inserting data to a file.java
+++ Handling/inserting data to a file.java
@@ -8,11 +8,14 @@ public static void main(String [] args) throws IOException
   
 {
   
+  
 File handling = new File("E:\\javaclasses\\handling\\hello.txt");
+
   
 FileWriter fw = new FileWriter(handling);
 fw.write("java is good");
 System.out.println();
+  
   
 fw.write("hello world");
 fw.write("get a job");
