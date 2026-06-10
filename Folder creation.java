86b781b2a754bf88864e2821593a2f80f9ab6e8d
Prathish S <prathish472@gmail.com>
2026-06-10 18:02:24 +0530

    Update Folder creation.java

--- Folder creation.java
+++ Folder creation.java
@@ -2,11 +2,18 @@ import java.io.File;
 public class FolderCreation{
 	public static void main(String [] args){
 		File file =new File("E:\\Java\\new");
+		
 		boolean isCreated = file.mkdir(); /*returns true iffolder create's else returns false*/
+		
 		if(isCreated){ /*isCreated = true or false*/
+			
 			System.out.println("Folder is created");
-		}else{
+			
+		}
+		else{
+			
 			System.out.println("Folder already exists");
+			
 		}
 	}
 }
