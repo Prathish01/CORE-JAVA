575da3b8fb0caa2d55fe03c78e43cafde9220018
Prathish S <prathish472@gmail.com>
2026-06-10 18:01:34 +0530

    Update threadname.java

--- Thread/threadname.java
+++ Thread/threadname.java
@@ -8,10 +8,11 @@ System.out.println("id of the usd :" +Thread.currentThread().getName());
 Thread.currentThread().setName("udt thread");
 System.out.println("id of the usd :" +Thread.currentThread().getName());	
 }
-
-
 public static void main(String [] args)
+  
 {
+  
+
   
 System.out.println(Thread.currentThread().getName());	
 Thread.currentThread().setName("main thread");
@@ -19,6 +20,7 @@ System.out.println("id of the main :" +Thread.currentThread().getName());
 getandset ti = new getandset();
 Thread t1 = new Thread(ti);
 t1.start();
+  
 }
 }
 
