5966f2fd177f6baf63e560ab5f6a0d9b9578c056
Prathish S <prathish472@gmail.com>
2026-06-10 18:01:14 +0530

    Update threadidstart.java

--- Thread/threadidstart.java
+++ Thread/threadidstart.java
@@ -6,16 +6,14 @@ public void run()
 {
 System.out.println("id of the usd :" +Thread.currentThread().getId());	
 }
-
-
 public static void main(String [] args)
 {
 
-  
 System.out.println(Thread.currentThread().getId());	
 threadid ti = new threadid();
   
 Thread t1 = new Thread(ti);
 t1.start();
+  
 }
 }
