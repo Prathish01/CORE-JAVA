987bdbc56a0ae56b90107d8b637afc79379c3288
Prathish S <prathish472@gmail.com>
2026-06-10 17:58:29 +0530

    Update byimplementingrunnable.java

--- Thread/byimplementingrunnable.java
+++ Thread/byimplementingrunnable.java
@@ -3,13 +3,20 @@ package thread;
 public class runmethod implements Runnable
 {
 public void run()
+  
 {
+  
 for(int i = 1;i<=20;i++)
+  
 {
+  
 System.out.println(i);	
+  
 }
 }
+  
 public static void main(String [] args)
+  
 {
 runmethod r1 = new runmethod();
 Thread t1 = new Thread(r1);
