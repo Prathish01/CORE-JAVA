d6f2de8206a02ef534fe08a49a0d4f523df97d3d
Prathish S <prathish472@gmail.com>
2026-06-10 18:04:00 +0530

    Update mp3filecreation.java

--- mp3filecreation.java
+++ mp3filecreation.java
@@ -2,8 +2,10 @@ package fh;
 import java.io.*;
 public class fle 
 {
+  
 public static void main(String [] args) throws IOException
 {
+  
 File flute = new File("C:\\Users\\mural\\OneDrive\\Desktop\\xx\\we\\dingi\\flute.mp3");
 boolean b = flute.createNewFile();
 if(b)
