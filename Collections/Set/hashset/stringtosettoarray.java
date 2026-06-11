d84b48fc6b6033c90241346a671d55b2e9dd371d
Prathish S <prathish472@gmail.com>
2026-06-11 17:36:10 +0530

    Update stringtosettoarray.java

--- Collections/Set/hashset/stringtosettoarray.java
+++ Collections/Set/hashset/stringtosettoarray.java
@@ -4,11 +4,12 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-
 public class stringtolisttoarray 
 {
+	
 public static void main(String [] args)
 {
+	
 System.out.println("creating a String");
 String s1 = "java is a programming language";
 	
@@ -16,8 +17,11 @@ String[] word = s1.split(" ");
 	
 Set<String> set = new HashSet<String>();
 for(String w :word)	
+	
 {
+	
 	set.add(w);
+	
 }
 //System.out.println(set);
 System.out.println("converting set into array");
