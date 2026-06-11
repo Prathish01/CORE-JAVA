a292a171d027133323c5cc39aa1bb73dc475c4ea
Prathish S <prathish472@gmail.com>
2026-06-11 17:34:46 +0530

    Update stacktoset.java

--- Collections/Set/hashset/stacktoset.java
+++ Collections/Set/hashset/stacktoset.java
@@ -18,14 +18,18 @@ Stack<Integer> list = new Stack<Integer>();
   
 list.add(10);
 list.add(20);	
+  
 list.add(30);	
 list.add(40);	
+  
 list.add(50);	
+  
 list.add(60);	
   
 list.add(70);	
 list.add(80);	
 list.add(90);	
+  
 list.add(100);	
 list.push(200);
 
