4f9e53ee7dc369f07273484d49ba68e14c39ce21
Prathish S <prathish472@gmail.com>
2026-06-19 21:18:19 +0530

    Update for loop example.java

--- for loop example.java
+++ for loop example.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 class test1
 {
+  
 public static void main(String [] args)
 {
+  
 Scanner scn = new Scanner(System.in);
 System.out.println("enter number");
 int num = scn.nextInt();
 int count = 0;
 m1(num,count);
 
+  
 }
 public static void m1(int num,int count)
 {
