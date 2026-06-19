6c23851091e63199c260f7e4e1eea8f1d76a8115
Prathish S <prathish472@gmail.com>
2026-06-19 21:21:52 +0530

    Update example.java

--- Array and programs/example.java
+++ Array and programs/example.java
@@ -4,15 +4,21 @@ public class text {
         Scanner scn =new Scanner(System.in); 
         System.out.println("enter a size"); 
         int size=scn.nextInt(); 
+
         
         int[] a1= new int[size]; 
         
+        
         System.out.println("enter the values"); 
         for(int i=0; i <=a1.length-1; i++) 
+
             
         { 
+
             
             a1[i]=scn.nextInt(); 
+
+            
         } 
         
         System.out.println("---result---"); 
