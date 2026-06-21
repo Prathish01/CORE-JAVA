b1125bd7b379b23f3a97ba1ab3db6cec1cf91efa
Prathish S <prathish472@gmail.com>
2026-06-21 08:01:52 +0530

    Update triangle pattern.java

--- triangle pattern.java
+++ triangle pattern.java
@@ -1,8 +1,7 @@
 package patternproblems;
-
-
 public class trianglepttern 
 {
+	
 	
 	public static void main(String [] args)
 	
@@ -21,6 +20,7 @@ for(int i =1;i<=4;i++)
 	System.out.println();
 }
 }
+	
 	}
 
 
