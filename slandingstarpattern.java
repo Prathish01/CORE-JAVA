e9529dea890669a7ccada3a0b14196f49978d88d
Prathish S <prathish472@gmail.com>
2026-06-21 08:03:17 +0530

    Update slandingstarpattern.java

--- slandingstarpattern.java
+++ slandingstarpattern.java
@@ -1,6 +1,4 @@
 package patternproblems;
-
-
 public class pattern9 {
     
     public static void main(String[] args) {
@@ -9,16 +7,23 @@ public class pattern9 {
             for (int j = 1; j <= i; j++) {
                 if (i == j) {
                     System.out.print("*");  // print star only at diagonal
+                    
                 } 
                 else 
+                    
                 {
                     System.out.print(" ");  // spaces elsewhere
+                    
                 }
             }
             System.out.println(); // new line after each row
+            
         }
+        
     }
 }
+
+
 
 output:
 
