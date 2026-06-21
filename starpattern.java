21cd5663c27d018a918720bab77eaf71e93d4851
Prathish S <prathish472@gmail.com>
2026-06-21 08:02:46 +0530

    Update starpattern.java

--- starpattern.java
+++ starpattern.java
@@ -7,6 +7,7 @@ public static void main(String [] args)
 	
 {
 	
+	
 	for(int i =1;i<=4;i++)
 	{
 		for(int j =1;j<=i;j++)
@@ -17,12 +18,16 @@ public static void main(String [] args)
 	}
 	
 for(int k=1;k<=4;k++)
+	
 {
 for(int m=4;m>=k;m--)
+	
 {
 System.out.print("*");
+	
 }
 System.out.println();
+	
 }
 }
 }
