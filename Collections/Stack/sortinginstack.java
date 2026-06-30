c55f9aad286134eabc0df9da5d4abb01a8417410
Prathish S <prathish472@gmail.com>
2026-06-30 17:20:56 +0530

    Update sortinginstack.java

--- Collections/Stack/sortinginstack.java
+++ Collections/Stack/sortinginstack.java
@@ -3,13 +3,17 @@ package stack;
 import java.util.Collections;
 import java.util.Stack;
 
-public class sort 
+public class sort
+	
 {
 public static void main(String [] args)
+	
 {
+	
 	Stack sta = new Stack();
 	sta.add(10);
 	sta.add(4545);
+	
 	sta.add(7);
 	sta.add(454);
 	
