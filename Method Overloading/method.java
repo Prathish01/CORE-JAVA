8fe9ec5046cb163cb90c0e443611fa631a42a503
Prathish S <prathish472@gmail.com>
2026-07-01 18:23:43 +0530

    Update method.java

--- Method Overloading/method.java
+++ Method Overloading/method.java
@@ -6,19 +6,23 @@ public class MethodOverloadingExample {
     }
 
    
+   
     public void display(String text) {
         System.out.println("Display method with string: " + text);
     }
 
    
+   
     public void display(int num, String text) {
         System.out.println("Display method with integer and string: " + num + ", " + text);
     }
+   
    
 
     public void display(String text, int num) {
         System.out.println("Display method with string and integer: " + text + ", " + num);
     }
+
    
 
     public static void main(String[] args) {
@@ -26,6 +30,8 @@ public class MethodOverloadingExample {
         example.display(10);                    
         example.display("Hello");               
         example.display(20, "World");           
-        example.display("Java", 30);            
+        example.display("Java", 30);    
+
+       
     }
 }
