2d4464c3edc46d52726a0667dbc24bb6c06d26f0
Prathish S <prathish472@gmail.com>
2026-07-03 21:34:50 +0530

    Update finally keyword.java

--- Exception/finally keyword.java
+++ Exception/finally keyword.java
@@ -4,9 +4,12 @@ public class finallykeyword
 {
 public static void main(String [] args)
 {
+
+
 System.out.println(1);
 System.out.println(2);
 try {
+
 	
 System.out.println(3);
 System.out.println(4/0);
