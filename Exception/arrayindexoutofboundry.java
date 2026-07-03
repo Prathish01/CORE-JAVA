9504209e10cf29144499abe7d2a6c138661b96b3
Prathish S <prathish472@gmail.com>
2026-07-03 21:33:10 +0530

    Update arrayindexoutofboundry.java

--- Exception/arrayindexoutofboundry.java
+++ Exception/arrayindexoutofboundry.java
@@ -5,16 +5,12 @@ public class arrayindexoutofboundry
 	
 public static void main(String [] args)
 {
-	
 	Scanner scn = new Scanner(System.in);
 int[] arr1 = {10,20,30,40,50};
 	
-
 		System.out.println(arr1[5]);
 }
-	
 }
-
 
 output
 Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 5 out of bounds for length 5
