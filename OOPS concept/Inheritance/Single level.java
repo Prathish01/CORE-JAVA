e2ddc6b8959958d64debbb3d768a9eb60ae12ef9
Prathish S <prathish472@gmail.com>
2026-07-04 19:55:36 +0530

    Update Single level.java

--- OOPS concept/Inheritance/Single level.java
+++ OOPS concept/Inheritance/Single level.java
@@ -1,15 +1,14 @@
 class Animal {
     void eat() {
         System.out.println("This animal eats food.");
+        
     }
 }
-
 class Dog extends Animal {
     void bark() {
         System.out.println("The dog barks.");
     }
 }
-
 public class TestInheritance {
     public static void main(String[] args) {
         Dog d = new Dog();
